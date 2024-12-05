3b421c534b3b1e54f9ebb81413198e9642dfe885
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2024-12-05 20:19:55 +0500

    min & max key value pair

--- 03_hashing/04_numberPro.py
+++ 03_hashing/04_numberPro.py
@@ -10,7 +10,6 @@ def main() -> None:
     hashMap: Dict[int, int] = {}
     for num in array:
         hashMap[num] = hashMap.setdefault(num, 0) + 1
-    
     # Handle queries to find frequency of numbers
     queries: int = int(input())
     while queries > 0:
@@ -18,6 +17,13 @@ def main() -> None:
         count: int = hashMap.get(number, 0)
         print(number, " has occured ", count, "times")
         queries -= 1
+    # Finding the min and max key-value pair
+    max_value = max(hashMap.values())
+    max_key_pair = max(hashMap.items(), key=lambda x: x[0])
+    min_value = min(hashMap.values())
+    min_key_pair = min(hashMap.items(), key=lambda x: x[0])
+    print(max_key_pair)
+    print(min_key_pair)
 
 
 if __name__ == "__main__":
